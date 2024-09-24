df281b13008d2938ebe42509a905e27be541bf34
hseok <17452867+hs7982@users.noreply.github.com>
2024-09-24 16:19:22 +0900

    frontend init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,39 @@
+import {useEffect, useRef, useState} from 'react'
+
+function App() {
+    const mapRef = useRef(null);
+    const lat = 37.3595704// 위도 숫자로 넣어주기
+    const lng = 127.105399// 경도 숫자로 넣어주기
+
+        useEffect(() => {
+            const { naver } = window;
+            if (mapRef.current && naver) {
+                const location = new naver.maps.LatLng(lat, lng);
+                const map = new naver.maps.Map(mapRef.current, {
+                    center: location,
+                    zoom: 15, // 지도 확대 정도
+                    scaleControl: true,
+                    mapDataControl: true,
+                    mapTypeControl: true,
+                    zoomControl: true,
+                    disableKineticPan: false,
+                    tileSpare: 6
+                });
+
+                navigator.geolocation.getCurrentPosition((position) => {
+                    map.panTo(new naver.maps.LatLng(position.coords.latitude, position.coords.longitude))
+                })
+
+            }
+        }, []);
+
+
+    return (
+        <>
+            <div ref={mapRef} style={{width: "100%", height: "100%"}}></div>
+        </>
+
+    );
+}
+
+export default App
